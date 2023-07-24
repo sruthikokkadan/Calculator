b6e9d4f3e4ef75f05731efddfb5333f06bb204cb
sruthikokkadan <sruthikokkadan@gmail.com>
2023-07-24 12:58:58 +0530

    calculator window with color and size created

---
+++ simple_calculator.py
@@ -0,0 +1,13 @@
+# import the required libraries
+from tkinter import *
+
+# create an instance of Tkinter window
+window = Tk()
+window.title("Calculator")
+# size and color of window
+window.geometry('225x300')
+window.configure(bg="#C0C0C0")
+window.resizable(0, 0)
+# To load calculator window
+window.mainloop()
+
