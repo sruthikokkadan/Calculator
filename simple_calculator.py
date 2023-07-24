67c158ef82183952865e42a2df5dbe033c5fa714
sruthikokkadan <sruthikokkadan@gmail.com>
2023-07-24 14:39:26 +0530

    input field entry

--- simple_calculator.py
+++ simple_calculator.py
@@ -8,6 +8,21 @@ window.title("Calculator")
 window.geometry('225x300')
 window.configure(bg="#C0C0C0")
 window.resizable(0, 0)
+
+# input field entry
+input_text = StringVar()
+input_field = Entry(window, textvariable=input_text, bg="#90EE90", bd="7px", fg="black",)
+input_field.grid(row="0", columnspan="4")
+expression = ""
+
+
+
+
+
+
+
+
+
 # To load calculator window
 window.mainloop()
 
